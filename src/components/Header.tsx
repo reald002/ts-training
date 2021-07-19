391b4e673a671aebf56c208c95ed0a25df2f826b
Andrew <zebelian.exceedteam@gmail.com>
2021-07-19 13:43:03 +0300

    init app

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  const text: string = 'Hello';
+
+  return (
+    <header>
+      {text}
+    </header>
+  );
+};
+
+export default Header;
